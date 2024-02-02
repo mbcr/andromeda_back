c455bfd0cb8374d3e6a756d0ec21cb12b382398d
Marcos Ramos <marcosbcr@yahoo.com.br>
2024-02-02 21:21:58 +0000

    [DEBUG] Fix the time_threshold_for_next_check on the check_unpaid_orders_for_payments task

--- apps/chainvet/tasks.py
+++ apps/chainvet/tasks.py
@@ -19,10 +19,10 @@ def check_payments():
         for unpaid_order in unpaid_orders:
             # minutes_since_last_update = unpaid_order.minutes_since_last_update()
             minutes_since_creation = unpaid_order.minutes_since_created()
-            time_threshold_for_next_check = unpaid_order.status_updated_at + timedelta(minutes= (3+ minutes_since_creation/20))
+            time_threshold_for_next_check = unpaid_order.status_updated_at + timedelta(minutes= 2) + minutes_since_creation/20
             if django_tz.now() > time_threshold_for_next_check:
                 unpaid_order.update_payment_status()
-            time.sleep(0.1)
+            time.sleep(0.05)
 
     except Exception as e:
         logger.exception("Error in fetch_messages: %s", e)
