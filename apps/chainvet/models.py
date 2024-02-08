4c138d798e66c6e4291ecc088d1790edec67803f
Marcos Ramos <marcosbcr@yahoo.com.br>
2024-02-08 16:30:34 +0000

    [DEBUG] Test Order status update with Trocador integration - Added the anonpayID to the Order str method
        WIP [DEBUG] Test Order status update with Trocador integration
            DONE Added the response json to the trocador api logger
            DONE Added the anonpayID to the Order str method

--- apps/chainvet/models.py
+++ apps/chainvet/models.py
@@ -56,7 +56,7 @@ class Order(models.Model):
         paid_date = self.paid_at.strftime('%Y.%m.%d %Hh%Mm%Ss') if self.paid_at else ''
         order_by_text = f"{self.owner_type} {self.access_code if self.owner_type == 'AccessCode' else self.user}"
         paid_text = f"{paid_date if self.is_paid else self.is_paid}"
-        return f'Order {self.id}.{self.order_id} - {self.created_at.strftime("%Y.%m.%d %Hh%Mm%Ss")} - {self.number_of_credits} credits - By: {order_by_text} - PAID: {paid_text}.'
+        return f'Order {self.id}.{self.order_id} - {self.created_at.strftime("%Y.%m.%d %Hh%Mm%Ss")} - {self.number_of_credits} credits - By: {order_by_text} - AnonpayID: {self.anonpay_id} - PAID: {paid_text}.'
 
     def generate_unique_code(self):
         length = 12
