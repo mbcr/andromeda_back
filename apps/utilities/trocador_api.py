f1c966109f86f508c2a44ca94a9cc4ecd0a02318
Marcos Ramos <marcosbcr@yahoo.com.br>
2024-02-08 16:26:51 +0000

    Added the response json to the trocador api logger

--- apps/utilities/trocador_api.py
+++ apps/utilities/trocador_api.py
@@ -8,5 +8,5 @@ logger = logging.getLogger('api_calls_trocador')
 def get_trade_status(trade_id: str):
     url = main_url + 'anonpay/status/' + trade_id
     response = requests.get(url)
-    logger.debug(f"trocador_api>get_trade_status: trade_id: {trade_id}, response_code: {response.status_code}")
+    logger.debug(f"trocador_api>get_trade_status: trade_id: {trade_id}, response_code: {response.status_code}, response_text: {response.json()}")
     return response.json()
