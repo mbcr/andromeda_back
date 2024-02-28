8db9ea9b253193edc8275f80022953e8d7ea42a9
Marcos Ramos <marcosbcr@yahoo.com.br>
2024-02-28 21:00:35 +0000

    [UPGRADE] Create representation field for risk grade (low, medium, high) for Assessments - Sample 5 successful transactions with Easybit
        WIP [UPGRADE] Create representation field for risk grade (low, medium, high) for Assessments
            DONE Created shenanigans.py in utilities to play with the data and infer how the risk grade is calculated
            DONE Research risk score calculation
            DONE Sample 5 successful transactions with Easybit
                TrocadorID Coin - Address - Tx Hash
                pTMJ0SNkrt BTC -  1LzYqd3q36cDzEixD2awi4BF3yvq1h66zC bbb38602cba93d42370ccf524ac9103561ec3779b497be0850c97d1d4ca3651e
                YjLpianeVs BTC -  12JE2Yht1n29kWWpkCWeVKGdxCq5XSQtpJ 7e4d6b45516518bd871f4b53a237520f8e3be00a13226e72dc16c00993b67e72
                27fH7T0Dn2 LTC -  LdwbE5tBdhivnAEZyAfBDf1uFYkM4vGyqP dc6c1a335b7e6246158b6ac8609fe794337885b251ec974f1836066518ebd28c
                UC8mnqMt0T ETH -  0x3dc4cedc2fb3e290bba13afed27233de801779b2 0x83615ae35e3a8b34a4da1d762bb24c63521a6a53d5c7440b9498b79dd2d20429
                9Ae41Swf5b BTC -  1Bi2RBZYxM2qospr99fnifpmQt2Bc5x268 09c65e794957c2a684356dd5873b5319e674d86467b405b5f35e02324574c5d5
                SVu2LqDD3X BTC -  bc1qqlh8w6s9j36ghhe6uz3agyahx5ngtudpq2xm94 32ce3cbe0c1e5b1fe281933c32bc31bb2834b7d024af9110f577e570a700d317 BITCOINVN
                s9W6iGkATf USDT -  TBb1MPf75WFTdcCDCKcwBacLqH4mkvu9Rc d23a5818c0e6b17650f12c5eff3721f405e5e8679ff50cdad5c4789ea3fca833 FAILED
                EVGtR7qMwn ETH -  0xa1fdce8770ee45ef8af48884adcb3f127b68c3f0 0x6e5417c40d0cd1ef801a5520e1d1f2ee1b3911085588bcb8b2f9de0fa4d15f89
            WIP Prepare function for determining risk grade

--- apps/utilities/shenanigans.py
+++ apps/utilities/shenanigans.py
@@ -1,63 +1,121 @@
 from pprint import pprint
 
-weights = {
-            "atm": 0.5,
-            "child_exploitation": 1,
-            "dark_market": 1,
-            "dark_service": 1,
-            "enforcement_action": 0.75,
-            "exchange_fraudulent": 1,
-            "exchange_licensed": 0.1,
-            "exchange_unlicensed": 0.6,
-            "gambling": 0.75,
-            "illegal_service": 1,
-            "liquidity_pools": 0.5,
-            "marketplace": 0,
-            "miner": 0,
-            "mixer": 1,
-            "other": 0.25,
-            "p2p_exchange_licensed": 0.1,
-            "p2p_exchange_unlicensed": 0.6,
-            "payment": 0,
-            "ransom": 1,
-            "sanctions": 1,
-            "scam": 1,
-            "seized_assets": 0,
-            "stolen_coins": 1,
-            "terrorism_financing": 1,
-            "wallet": 0.25
-        }
-signals = {
-            "atm": 0,
-            "child_exploitation": 0,
-            "dark_market": 0,
-            "dark_service": 0,
-            "enforcement_action": 0,
-            "exchange_fraudulent": 0.028,
-            "exchange_licensed": 0.217,
-            "exchange_unlicensed": 0.147,
-            "gambling": 0.003,
-            "illegal_service": 0,
-            "liquidity_pools": 0.003,
-            "marketplace": 0,
-            "miner": 0,
-            "mixer": 0,
-            "other": 0.27,
-            "p2p_exchange_licensed": 0,
-            "p2p_exchange_unlicensed": 0.153,
-            "payment": 0.004,
-            "ransom": 0,
-            "sanctions": 0,
-            "scam": 0.021,
-            "seized_assets": 0,
-            "stolen_coins": 0.144,
-            "terrorism_financing": 0,
-            "wallet": 0.008
-        }
 
-aggregator = 0
-for key, value in weights.items():
-    print(key, value, signals[key])
-    aggregator += signals[key] * value
+def assess_risk_grade():
+    weights = {
+                "atm": 0.5,
+                "child_exploitation": 1,
+                "dark_market": 1,
+                "dark_service": 1,
+                "enforcement_action": 0.75,
+                "exchange_fraudulent": 1,
+                "exchange_licensed": 0.1,
+                "exchange_unlicensed": 0.6,
+                "gambling": 0.75,
+                "illegal_service": 1,
+                "liquidity_pools": 0.5,
+                "marketplace": 0,
+                "miner": 0,
+                "mixer": 1,
+                "other": 0.25,
+                "p2p_exchange_licensed": 0.1,
+                "p2p_exchange_unlicensed": 0.6,
+                "payment": 0,
+                "ransom": 1,
+                "sanctions": 1,
+                "scam": 1,
+                "seized_assets": 0,
+                "stolen_coins": 1,
+                "terrorism_financing": 1,
+                "wallet": 0.25
+            }
+    signals = {
+                "atm": 0,
+                "child_exploitation": 0,
+                "dark_market": 0,
+                "dark_service": 0,
+                "enforcement_action": 0,
+                "exchange_fraudulent": 0.028,
+                "exchange_licensed": 0.217,
+                "exchange_unlicensed": 0.147,
+                "gambling": 0.003,
+                "illegal_service": 0,
+                "liquidity_pools": 0.003,
+                "marketplace": 0,
+                "miner": 0,
+                "mixer": 0,
+                "other": 0.27,
+                "p2p_exchange_licensed": 0,
+                "p2p_exchange_unlicensed": 0.153,
+                "payment": 0.004,
+                "ransom": 0,
+                "sanctions": 0,
+                "scam": 0.021,
+                "seized_assets": 0,
+                "stolen_coins": 0.144,
+                "terrorism_financing": 0,
+                "wallet": 0.008
+            }
 
-print(f'Total: {aggregator}')
+    risk_thresholds = {
+        "atm": (0.1, 0.2),
+        "child_exploitation": (0.002, 0.01),
+        "dark_market": (0.01, 0.04),
+        "dark_service": (0.01, 0.04),
+        "enforcement_action": (0.02, 0.04),
+        "exchange_fraudulent": (0.05, 0.1),
+        "exchange_unlicensed": (0.2, 0.4),
+        "gambling": (0.02, 0.04),
+        "illegal_service": (0.02, 0.04),
+        "liquidity_pools": (0.04, 0.12),
+        "mixer": (0.02, 0.06),
+        "other": (0.2, 0.6),
+        "p2p_exchange_unlicensed": (0.1, 0.3),
+        "ransom": (0.002, 0.02),
+        "sanctions": (0.002, 0.01),
+        "scam": (0.002, 0.02),
+        "stolen_coins": (0.002, 0.02),
+        "terrorism_financing": (0.002, 0.01),
+        "wallet": (0.4, 0.8),
+    }
+
+    weighed_signals = {}
+
+    aggregator = 0
+    for key, value in weights.items():
+        # print(key, value, signals[key])
+        specific_risk = signals[key] * value
+        aggregator += specific_risk
+        weighed_signals[key] = specific_risk
+        
+
+    print(f'Total: {aggregator}')
+    pprint(weighed_signals)
+
+
+def get_low_risk_thresholds():
+    low_risk_thresholds = {}
+
+    signals_sampling_from_successful_transactions = [
+        {"atm": 0.0, "scam": 0.0, "miner": 0.0, "mixer": 0.0, "other": 0.0, "ransom": 0.0, "wallet": 0.0, "payment": 0.0, "gambling": 0.0, "sanctions": 0.0, "dark_market": 0.0, "marketplace": 0.0, "dark_service": 0.0, "stolen_coins": 0.0, "seized_assets": 0.0, "illegal_service": 0.0, "liquidity_pools": 0.0, "exchange_licensed": 0.0, "child_exploitation": 0.0, "enforcement_action": 0.0, "exchange_fraudulent": 0.0, "exchange_unlicensed": 1.0, "terrorism_financing": 0.0, "p2p_exchange_licensed": 0.0, "p2p_exchange_unlicensed": 0.0},
+        {"atm": 0.0, "scam": 0.0, "miner": 0.0, "mixer": 0.0, "other": 0.0, "ransom": 0.0, "wallet": 0.0, "payment": 0.0, "gambling": 0.0, "sanctions": 0.0, "dark_market": 0.0, "marketplace": 0.0, "dark_service": 0.0, "stolen_coins": 0.0, "seized_assets": 0.0, "illegal_service": 0.0, "liquidity_pools": 0.0, "exchange_licensed": 0.0, "child_exploitation": 0.0, "enforcement_action": 0.0, "exchange_fraudulent": 0.0, "exchange_unlicensed": 1.0, "terrorism_financing": 0.0, "p2p_exchange_licensed": 0.0, "p2p_exchange_unlicensed": 0.0},
+        {"atm": 0.0, "scam": 0.0, "miner": 0.0, "mixer": 0.0, "other": 0.0, "ransom": 0.0, "wallet": 0.0, "payment": 0.042, "gambling": 0.013, "sanctions": 0.0, "dark_market": 0.0, "marketplace": 0.0, "dark_service": 0.0, "stolen_coins": 0.0, "seized_assets": 0.0, "illegal_service": 0.0, "liquidity_pools": 0.0, "exchange_licensed": 0.932, "child_exploitation": 0.0, "enforcement_action": 0.0, "exchange_fraudulent": 0.0, "exchange_unlicensed": 0.01, "terrorism_financing": 0.0, "p2p_exchange_licensed": 0.0, "p2p_exchange_unlicensed": 0.0},
+        {"atm": 0.0, "scam": 0.0, "miner": 0.003, "mixer": 0.003, "other": 0.014, "ransom": 0.0, "wallet": 0.083, "payment": 0.092, "gambling": 0.001, "sanctions": 0.0, "dark_market": 0.0, "marketplace": 0.0, "dark_service": 0.0, "stolen_coins": 0.0, "seized_assets": 0.0, "illegal_service": 0.0, "liquidity_pools": 0.0, "exchange_licensed": 0.766, "child_exploitation": 0.0, "enforcement_action": 0.0, "exchange_fraudulent": 0.0, "exchange_unlicensed": 0.034, "terrorism_financing": 0.0, "p2p_exchange_licensed": 0.001, "p2p_exchange_unlicensed": 0.0},
+        {"atm": 0.001, "scam": 0.0, "miner": 0.007, "mixer": 0.003, "other": 0.011, "ransom": 0.0, "wallet": 0.011, "payment": 0.027, "gambling": 0.02, "sanctions": 0.0, "dark_market": 0.006, "marketplace": 0.0, "dark_service": 0.0, "stolen_coins": 0.001, "seized_assets": 0.0, "illegal_service": 0.0, "liquidity_pools": 0.0, "exchange_licensed": 0.769, "child_exploitation": 0.0, "enforcement_action": 0.012, "exchange_fraudulent": 0.0, "exchange_unlicensed": 0.122, "terrorism_financing": 0.0, "p2p_exchange_licensed": 0.003, "p2p_exchange_unlicensed": 0.003},
+        {"atm": 0.0, "scam": 0.001, "miner": 0.003, "mixer": 0.0, "other": 0.004, "ransom": 0.0, "wallet": 0.013, "payment": 0.033, "gambling": 0.002, "sanctions": 0.0, "dark_market": 0.0, "marketplace": 0.0, "dark_service": 0.0, "stolen_coins": 0.0, "seized_assets": 0.0, "illegal_service": 0.0, "liquidity_pools": 0.0, "exchange_licensed": 0.63, "child_exploitation": 0.0, "enforcement_action": 0.001, "exchange_fraudulent": 0.0, "exchange_unlicensed": 0.257, "terrorism_financing": 0.0, "p2p_exchange_licensed": 0.047, "p2p_exchange_unlicensed": 0.006},
+        {"atm": 0.0, "scam": 0.0, "miner": 0.0, "mixer": 0.0, "other": 0.601, "ransom": 0.0, "wallet": 0.003, "payment": 0.0, "gambling": 0.0, "sanctions": 0.0, "dark_market": 0.0, "marketplace": 0.003, "dark_service": 0.0, "stolen_coins": 0.0, "seized_assets": 0.0, "illegal_service": 0.0, "liquidity_pools": 0.019, "exchange_licensed": 0.045, "child_exploitation": 0.0, "enforcement_action": 0.0, "exchange_fraudulent": 0.0, "exchange_unlicensed": 0.004, "terrorism_financing": 0.0, "p2p_exchange_licensed": 0.0, "p2p_exchange_unlicensed": 0.324},    
+    ]
+
+    for item in signals_sampling_from_successful_transactions:
+        for key, value in item.items():
+            if key not in low_risk_thresholds:
+                if value:
+                    low_risk_thresholds[key] = value
+            else:
+                low_risk_thresholds[key] = max(low_risk_thresholds[key], value)
+    
+    pprint(low_risk_thresholds)
+
+get_low_risk_thresholds()
+
+
